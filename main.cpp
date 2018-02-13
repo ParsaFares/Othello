84d03a7859158f1bcc214b3aa103236ae66bf343
Parsa <parsa1997.ya@gmail.com>
2018-02-13 09:20:18 +0330

    Change main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <QApplication>
+#include <QtGui>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "MainWindow.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app (argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
 }
